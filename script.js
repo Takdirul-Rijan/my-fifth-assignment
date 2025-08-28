b47677e62aba36806bce02dc32291011a7820e9a
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-08-29 02:08:30 +0600

    functionalities for click the copy button added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Functionalities for click the heart icon to increase the count
+// 1. functionalities for click the heart icon to increase the count
 
 const heartCountEl = document.getElementById("heart-count");
 
@@ -23,3 +23,31 @@ function increaseHeartCount(heartCountEl, buttonSelector) {
   }
 }
 increaseHeartCount(heartCountEl, ".btn-heart-click");
+
+// 2. functionalities for click the copy button to increase the copy count and copy hotline number
+
+const copyCountEl = document.getElementById("copy-count");
+
+const allCopyButtons = document.querySelectorAll(".btn-copy-click");
+
+for (const btn of allCopyButtons) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const card = btn.closest(".card-body");
+
+    const title = card.querySelector(".card-title").innerText;
+
+    const hotline = card.querySelector(".hotline-number").innerText;
+
+    navigator.clipboard.writeText(hotline);
+
+    alert(title + "\n" + hotline);
+
+    let current = parseInt(copyCountEl.innerText);
+
+    if (isNaN(current)) current = 0;
+
+    copyCountEl.innerText = current + 1;
+  });
+}
