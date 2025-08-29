9eb049446957e5d44f098d7270a2ae404e53bea5
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-08-29 06:10:04 +0600

    added functionalities for click the call button

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function increaseHeartCount(heartCountEl, buttonSelector) {
       heartCountEl.innerText = current + 1;
 
       // --- change icon style ---
-      const icon = btn.querySelector("i"); // find <i> inside this button
+      const icon = btn.querySelector("i");
       if (icon && icon.classList.contains("fa-regular")) {
         icon.classList.remove("fa-regular");
         icon.classList.add("fa-solid");
@@ -51,3 +51,56 @@ for (const btn of allCopyButtons) {
     copyCountEl.innerText = current + 1;
   });
 }
+
+// 3. functionalities for click the call button
+
+const coinCountEl = document.getElementById("coin-count");
+
+const allCallButtons = document.querySelectorAll(".btn-call-click");
+
+const callHistoryListEl = document.getElementById("history-list");
+
+for (const btn of allCallButtons) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const card = btn.closest(".card-body");
+
+    const title = card.querySelector(".card-title").innerText;
+
+    const hotline = card.querySelector(".hotline-number").innerText;
+
+    let current = parseInt(coinCountEl.innerText);
+    if (isNaN(current)) current = 100;
+
+    if (current < 20) {
+      alert("You need at least 20 coins to make a call.");
+      return;
+    }
+
+    alert("Calling...\n" + title + "\n" + hotline);
+
+    coinCountEl.innerText = current - 20;
+
+    const exactTime = new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+
+    const li = document.createElement("li");
+
+    li.classList.add("p-2", "bg-gray-100", "rounded");
+
+    li.innerText = `${title}\n${hotline}\nCalled at ${exactTime}`;
+
+    callHistoryListEl.prepend(li);
+  });
+}
+
+const clearHistoryBtn = document.getElementById("clear-history-btn");
+
+clearHistoryBtn.addEventListener("click", function () {
+  callHistoryListEl.innerHTML = "";
+});
